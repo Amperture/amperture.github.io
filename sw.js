79fa29fe4abc7684130a53aa1915d07188dd1582
W. Alex Best <amp@amperture.com>
2019-04-23 15:28:52 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-233c9745d77ab4d6d912.js"
+    "url": "webpack-runtime-284e5d096e37a95805ff.js"
   },
   {
     "url": "app-ff18b9fee035e2e6502e.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "217c2c9208bbcc00b374aaf5439d4068"
+    "revision": "174a99d1a4f17712602edc77632d99b1"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
